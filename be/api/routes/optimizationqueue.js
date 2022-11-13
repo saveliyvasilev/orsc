fdc23d80411694c833a3f0d88b8a6cdf847f8718
saveliyvasilev <saveliy.v.vasilev@gmail.com>
2022-11-13 19:56:12 -0300

    Add rabbitmq to docker-compose and stub for amqplib endpoint

--- be/api/routes/optimizationqueue.js
+++ be/api/routes/optimizationqueue.js
@@ -1,5 +1,23 @@
+const amqp = require('amqplib')
 const express = require('express')
+
 const router = express.Router()
+
+const scenario = { name: "MyName" }
+
+async function connectAmqp() {
+    try {
+        const amqpConnection = await amqp.connect(`amqp://guest:guest:rabbitmq:5672`);
+        const channel = await amqpConnection.createChannel();
+        const result = await channel.assertQueue("blendingScenario");
+        channel.sendToQueue("blendingScenario", Buffer.from(JSON.stringify(scenario)));
+        console.log("Enqueued " + scenario.name)
+    } catch (ex) {
+        console.log(ex)
+    }
+}
+connectAmqp();
+// amqp.
 
 
 router.post('/', (req, res) => {
