e4f2e11c507bfb2b8022453b28e6407c10b30826
saveliyvasilev <saveliy.v.vasilev@gmail.com>
2022-11-17 23:41:42 -0300

    Add blending worker stub to dockerfile

--- be/workers/blending/main.py
+++ be/workers/blending/main.py
@@ -1,5 +1,6 @@
 # from pprint import pprint
 from workers import AMQPWorker
+import time
 
 
 def fn_solve(raw_input):
@@ -7,14 +8,15 @@ def fn_solve(raw_input):
     return {
         "result": "I love you but I cannot do much.",
         "status": "Done",
-        "name": "Allo!",
+        "name": raw_input["name"],
     }
 
 
 if __name__ == "__main__":
+    time.sleep(5)  # To let docker rabbitmq begin
     worker = AMQPWorker(
         fn_solve,
-        amqp_host="localhost",
+        amqp_host="rabbitmq",  # Use localhost if executing the main.py outside a container
         input_queue="blendingScenario",
         output_queue="blendingSolution",
     )
