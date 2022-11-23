e04307800d7f1ef761c338c22fc3d557a98ea39b
saveliyvasilev <saveliy.v.vasilev@gmail.com>
2022-11-23 16:27:42 -0300

    Add convenience code for running python worker locally

--- be/workers/blending/main.py
+++ be/workers/blending/main.py
@@ -1,6 +1,16 @@
 # from pprint import pprint
 from workers import AMQPWorker
 import time
+import socket
+
+
+def amqp_host() -> str:
+    # TODO: This is super hacky, use environment variables instead
+    try:
+        socket.gethostbyname("rabbitmq")
+        return "rabbitmq"
+    except socket.error:
+        return "localhost"
 
 
 def fn_solve(raw_input):
@@ -14,10 +24,13 @@ def fn_solve(raw_input):
 
 
 if __name__ == "__main__":
-    time.sleep(5)  # To let docker rabbitmq begin
+    # TODO: This is a hack # To let docker rabbitmq begin
+    if amqp_host() == "rabbitmq":
+        time.sleep(5)
+    # End-hack
     worker = AMQPWorker(
         fn_solve,
-        amqp_host="rabbitmq",  # Use localhost if executing the main.py outside a container
+        amqp_host=amqp_host(),
         input_queue="blendingScenario",
         output_queue="blendingSolution",
     )
