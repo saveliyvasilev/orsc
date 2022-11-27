4b4bd8218efd0c17567d4be6e69bb0806d36574b
saveliyvasilev <saveliy.v.vasilev@gmail.com>
2022-11-26 21:47:16 -0300

    Add blending KPIs to json for populating the scenario overview

---
+++ be/workers/blending/output/kpis.py
@@ -0,0 +1,28 @@
+from typing import Dict
+from ..model_types import ModelOutput
+
+
+def kpis(model_output: ModelOutput) -> Dict:
+    print("KPISSS")
+    return {
+        "total_demand": _total_demand(model_output),
+        "total_underload": _total_underload(model_output),
+        "total_product_cost": _total_product_cost(model_output),
+        # "total_off_spec_orders": _total_off_spec_orders(model_output)
+    }
+
+
+def _total_demand(model_output: ModelOutput) -> float:
+    return sum(model_output.model_input.coefficients.demand.values())
+
+
+def _total_underload(model_output: ModelOutput) -> float:
+    return sum(model_output.variables.underload.values())
+
+
+def _total_product_cost(model_output: ModelOutput) -> float:
+    load = model_output.variables.load
+    return sum(
+        amount * model_output.model_input.coefficients.product_cost[product]
+        for (product, order), amount in load.items()
+    )
--- be/workers/blending/output/output.py
+++ be/workers/blending/output/output.py
@@ -1,7 +1,8 @@
 from typing import Dict
 from ..model_types import ModelOutput
+from .kpis import kpis
 from .orders import orders
 
 
 def build_json_model_output(model_output: ModelOutput) -> Dict:
-    return {"orders": orders(model_output)}
+    return {"kpis": kpis(model_output), "orders": orders(model_output)}
